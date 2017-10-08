5f56a677f4f6e403e530a353bd5d12b76425e69a
Felipe Cadavid <cadavid.felipe@hotmail.com>
2017-10-08 15:39:42 -0500

    Getter y -Setter Persona

--- src/segurosservir/Persona.java
+++ src/segurosservir/Persona.java
@@ -13,12 +13,44 @@ import java.util.Date;
  */
 public abstract class Persona {
     
-    String nombre;
-    String apellido;
-    int edad;
-    Date fechaNacimiento;
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private Date fechaNacimiento;
 
     public Persona() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
     }
     
     
