af394639e5dfd87970d23bc479c8dcace90fdf0e
JeisonVelasqC <jeisond.velasquez@udea.edu.co>
2017-10-10 00:37:58 -0500

    culedad

--- src/segurosservir/clientes/Clientes.java
+++ src/segurosservir/clientes/Clientes.java
@@ -42,17 +42,17 @@ public class Clientes {
         String hoy = formato.format(fechaActual);
         String[] dat1 = fecha_nac.split("/"); 
         String[] dat2 = hoy.split("/");
-        int anos = Integer.parseInt(dat2[2]) - Integer.parseInt(dat1[2]);
+        int años = Integer.parseInt(dat2[2]) - Integer.parseInt(dat1[2]);
         int mes = Integer.parseInt(dat2[1]) - Integer.parseInt(dat1[1]);
         if (mes < 0) {
-          anos = anos - 1;
+          años = años - 1;
         } else if (mes == 0) {
           int dia = Integer.parseInt(dat2[0]) - Integer.parseInt(dat1[0]);
           if (dia > 0) {
-            anos = anos - 1;
+            años = años - 1;
           }
         }
-        return anos;
+        return años;
   }
     
     public String getNombre() {
