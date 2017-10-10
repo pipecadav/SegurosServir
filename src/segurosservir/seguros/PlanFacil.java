7b2d68926ea28cbf92e5d2092b4bbf13ac1ef968
Felipe Cadavid <cadavid.felipe@hotmail.com>
2017-10-09 22:38:32 -0500

    Plannes Seguro de Vida

--- src/segurosservir/seguros/PlanFacil.java
+++ src/segurosservir/seguros/PlanFacil.java
@@ -1,6 +1,7 @@
 /*
  * Esta clase permite crear una poliza Total Mixta, donde el cliente 
- * asegurado recibe una prima neta única dentro de una cantidad de años n
+ * asegurado paga una prima neta única para un tiempo futuro en años n
+ * El asegurado recibe la poliza despues de n años 
  */
 package segurosservir.seguros;
 
@@ -17,13 +18,23 @@ public class PlanFacil implements SeguroVida{
     double prima;
     static final int EXPECTATIVAVIDA = 78;
     
-    
+    /**
+     * Valida los requisitos del asegurado, debe ser mayor o igual a 22 años
+     * Debe ser como mínimo 10 años menor que la expectativa de vida
+     * @return Devuelve verdadero o falso
+     */
     @Override
     public boolean validarViabilidad() {
-           return (((EXPECTATIVAVIDA - persona.getEdad()) <5) && (persona.getEdad() > 22));
+           return (((EXPECTATIVAVIDA - persona.getEdad()) <10) && (persona.getEdad() >= 22));
        }
   
 
+    /**
+     * Ejecuta la función Ax = Monto*(Dxn/Dx)
+     * @param montoAsegurar Cantidad a asegurar
+     * @param tiempoSeguro Tiempo futuro
+     * @return Retorna el valor de la prima
+     */
     @Override
     public double calculaCostoPrima(double montoAsegurar, int tiempoSeguro) {
         double dxn = 0.0;
@@ -40,6 +51,11 @@ public class PlanFacil implements SeguroVida{
         return prima;
     }
 
+    /**
+     * Difiere la prima en cuotas de 60 meses independiente del tiempo futuro
+     * @param prima Recibe el valor neto de la prima
+     * @return Retorna el valor a pagar mensual
+     */
     @Override
     public double calcularMensual(double prima) {
         return (prima/60);
--- src/segurosservir/seguros/PlanTranquilo.java
+++ src/segurosservir/seguros/PlanTranquilo.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Esta clase permite crear una poliza Temporal, 
+ * El usuario paga una poliza por un periodo determinado de tiempo de n años/meses 
+ * El asegurado accede a la poliza si muere en cualquier momento.
  */
 package segurosservir.seguros;
 
@@ -16,30 +16,46 @@ public class PlanTranquilo implements SeguroVida {
     static final int EXPECTATIVAVIDA = 78;
     int tiempoSeguro;
     
+    /**
+     * Valida los requisitos del asegurado, debe ser mayor o igual a 22 años
+     * Debe ser n+1 años menor a la expectativa, siento n el tiempo futuro
+     * @return Devuelve verdadero o falso
+     */
     @Override
     public boolean validarViabilidad() {
-           return (((EXPECTATIVAVIDA - persona.getEdad()) <20) && (persona.getEdad() > 22));
+           return (((EXPECTATIVAVIDA - persona.getEdad()) <(tiempoSeguro+1)) && (persona.getEdad() >= 22));
     }
 
+    /**
+     * Ejecuta la función Ax = Monto*((Mx - Mx+n)/Dx)
+     * @param montoAsegurar Cantidad a asegurar
+     * @param tiempoSeguro Tiempo futuro
+     * @return Retorna el valor de la prima
+     */
     @Override
     public double calculaCostoPrima(double montoAsegurar, int tiempoSeguro) {
         this.tiempoSeguro = tiempoSeguro;
         double mx = 0.0;
         double mxn = 0.0;
-        double dxn = 0.0;
+        double dx = 0.0;
         TablasDeReferencia.inicializarTabla();
         for(int i = 0; i < TablasDeReferencia.valores.size(); i++){
             if(persona.getEdad() == TablasDeReferencia.valores.get(i).getEdad()){
                 mx = TablasDeReferencia.valores.get(i).getMx();
                 mxn = TablasDeReferencia.valores.get(i+tiempoSeguro).getMx();
-                dxn = TablasDeReferencia.valores.get(i+tiempoSeguro).getDx();
+                dx = TablasDeReferencia.valores.get(i).getDx();
                 i = TablasDeReferencia.valores.size();
             }
         }
-        prima = montoAsegurar*((mx-mxn)/dxn);
+        prima = montoAsegurar*((mx-mxn)/dx);
         return prima;
     }
 
+    /**
+     * Difiere la prima en cuotas mensuales dependiendo del tiempo del seguro
+     * @param prima Recibe el valor neto de la prima
+     * @return Retorna el valor a pagar mensual
+     */
     @Override
     public double calcularMensual(double prima) {
         return (prima/(tiempoSeguro*12));
